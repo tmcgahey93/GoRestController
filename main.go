7e5490fc522e7ec703f55b8c721685583c0511d0
Troy McGahey <troymcgahey@Troys-MacBook-Pro.local>
2025-02-02 17:08:37 -0500

    Added second entry point

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 
 func GoRestControllerHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Troy's Go RestController Running")
+
+}
+
+func GoRestControllerHandler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Troy's Go RestController Second Entry Point Runnin")
 }
 
 func main() {
 	http.HandleFunc("/", GoRestControllerHandler)
+
+	http.HandleFunc("/anotherentrypoint", GoRestControllerHandler2)
 
 	fmt.Println("Server Running on portg 8080....")
 	log.Fatal(http.ListenAndServe(":8080", nil))
